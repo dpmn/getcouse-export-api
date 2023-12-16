e4a5ed233dd7d514936e4e41cac182ce22e0f09a
Oleg Ionov <ionov.dpmn@gmail.com>
2023-12-16 16:46:52 +0300

    Initial commit

--- getcourse_export_api/client.py
+++ getcourse_export_api/client.py
@@ -1,19 +1,15 @@
-import requests
-import time
-
+from requests import request as http_request
+from time import sleep
 from getcourse_export_api.exceptions import GetcourseApiError
-
-
-BASE_URL = 'https://{account_name}.getcourse.ru/pl/api'
 
 
 class Getcourse:
     def __init__(self, account_name: str, secret_key: str):
         self.account_name = account_name
         self._secret_key = secret_key
-        self._api_base_url = BASE_URL.format(account_name=account_name)
+        self._api_base_url = f'https://{account_name}.getcourse.ru/pl/api'
 
-    def _send_request(self, url, filters=None, delay=5, attempts=5):
+    def _send_request(self, url, filters=None, delay=5):
         if filters:
             params = {
                 'key': self._secret_key,
@@ -24,26 +20,19 @@ class Getcourse:
                 'key': self._secret_key
             }
 
-        response = requests.request('GET', url=url, params=params)
+        while True:
+            response = http_request('GET', url=url, params=params)
 
-        if response.status_code == 200:
-            response_json = response.json()
-            success = response_json.get('success', False)
+            if response.status_code == 200:
+                response_json = response.json()
+                success = response_json.get('success', False)
 
-            if success:
-                return response_json
-            elif attempts > 0:
-                attempts = attempts - 1
-                time.sleep(delay)
-
-                return self._send_request(url, filters, delay, attempts)
+                if success:
+                    return response_json
+                else:
+                    sleep(delay)
             else:
-                raise GetcourseApiError(response.content)
-        else:
-            attempts = attempts - 1
-            time.sleep(delay)
-
-            return self._send_request(url, filters, delay, attempts)
+                GetcourseApiError(response.status_code, response.text)
 
     @staticmethod
     def normalize_response(data):
@@ -62,15 +51,15 @@ class Getcourse:
 
         return normalize_result
 
-    def export(self, action: str, filters: dict, delay: int = 30):
+    def export(self, action: str, filters: dict):
         base_url = '/'.join([self._api_base_url, 'account'])
         action_url = '/'.join([base_url, action])
 
         # Запуск задачи на сбор данных
-        action_response = self._send_request(action_url, filters, attempts=1)
+        action_response = self._send_request(action_url, filters)
         export_id = str(action_response.get('info', {}).get('export_id', 0))
         export_url = '/'.join([base_url, 'exports', export_id])
 
         # Запрос на экспорт данных
-        export_response = self._send_request(export_url, delay=delay)
+        export_response = self._send_request(export_url)
         return export_response
